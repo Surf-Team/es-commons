70ddcf43993010d6a5026bafbe0811d7476a06a4
saniller <saniller@bk.ru>
2021-01-25 00:40:20 +0500

    changes

--- src/main/java/ru/es/lang/ESConstructor.java
+++ src/main/java/ru/es/lang/ESConstructor.java
@@ -3,7 +3,7 @@ package ru.es.lang;
 /**
  * Created by saniller on 30.06.2016.
  */
-public abstract class ESConstructor<NewObject, FromThis>
+public interface ESConstructor<NewObject, FromThis>
 {
-    public abstract NewObject createObject(FromThis... fromThis);
+    public NewObject createObject(FromThis fromThis);
 }
