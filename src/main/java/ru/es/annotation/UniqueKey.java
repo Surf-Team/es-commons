b30f535defe019f10e1d78fdd3bcfdd148fb6391
Alexander Sazz <saniller@bk.ru>
2022-08-08 16:12:28 +0500

    Аннотация UniqueKey: добавлено свойство autoIncrement

--- src/main/java/ru/es/annotation/UniqueKey.java
+++ src/main/java/ru/es/annotation/UniqueKey.java
@@ -9,5 +9,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface UniqueKey
 {
-
+	// если TRUE, то для SQL поле будет считаться как autoIncreasement
+	// если поле == Integer.MIN_VALUE, то оно не будет сохранено, то есть будет добавлено новое
+	boolean autoIncrement() default false;
 }
