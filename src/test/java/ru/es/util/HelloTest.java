effca40678e639b061d2db8e2c6240e2452604a5
Alexander Sazz <saniller@bk.ru>
2022-10-18 20:09:37 +0500

    Create HelloTest.java

---
+++ src/test/java/ru/es/util/HelloTest.java
@@ -0,0 +1,13 @@
+package ru.es.util;
+
+import org.junit.jupiter.api.Test;
+import ru.es.log.Log;
+
+public class HelloTest
+{
+	@Test
+	public void testHello()
+	{
+		Log.warning("Hello from es-commons!");
+	}
+}
