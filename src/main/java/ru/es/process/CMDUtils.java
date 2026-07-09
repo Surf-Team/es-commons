d1486988ad85cbbd98a611cec047e58892fc3f8e
Alexander Sazz <sanillerz@gmail.com>
2026-07-09 14:00:13 +0500

    Revert "update zip"
    This reverts commit 2549b1f9d2e2d178a3157886b79a5162d539d524.

--- src/main/java/ru/es/process/CMDUtils.java
+++ src/main/java/ru/es/process/CMDUtils.java
@@ -3,13 +3,9 @@ package ru.es.process;
 import ru.es.log.Log;
 import ru.es.util.Environment;
 
-import java.io.*;
+import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 public class CMDUtils
 {
@@ -41,126 +37,67 @@ public class CMDUtils
 	public static void addToArchive(ProcessFactory processFactory, String functionDesc,
 									File launchDirectory, File archiveFile, File directoryToArchivate) throws Exception
 	{
-		if (Environment.isWindows())
-		{
-			ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.addToArchive");
-			processInfo.addToPathEnv.add(new File("./7-Zip/").getAbsoluteFile().getAbsolutePath());
-			processFactory.createProcess(launchDirectory, processInfo, "CMD", "/C", "7z.exe", "a", "-tzip", archiveFile.getPath(), directoryToArchivate.getAbsolutePath());
-			if (processInfo.error)
-			{
-				Log.warning(processInfo.getStdout());
-				Log.warning(processInfo.getErrOut());
-				throw new Exception("Ошибка создания архива\r\n" + processInfo.getErrOut());
-			}
-		}
-		else
-		{
-			zipDirectoryToFile(directoryToArchivate, archiveFile);
-		}
+		ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.addToArchive");
+		processInfo.addToPathEnv.add(new File("./7-Zip/").getAbsoluteFile().getAbsolutePath());
 
+		if (Environment.isWindows())
+			processFactory.createProcess(launchDirectory, processInfo, "CMD", "/C", "7z.exe", "a", "-tzip", archiveFile.getPath(), directoryToArchivate.getAbsolutePath());
+		else
+			processFactory.createProcess(launchDirectory, processInfo, "zip", "-r", archiveFile.getPath(), directoryToArchivate.getAbsolutePath());
+
+
+		if (processInfo.error)
+		{
+			Log.warning(processInfo.getStdout());
+			Log.warning(processInfo.getErrOut());
+			throw new Exception("Ошибка создания архива\r\n"+processInfo.getErrOut());
+		}
 		if (!archiveFile.exists())
-			throw new Exception("Архив не был создан");
+		{
+			Log.warning(processInfo.getStdout());
+			Log.warning(processInfo.getErrOut());
+			throw new Exception("Архив не был создан\r\n"+processInfo.getErrOut());
+		}
 	}
 
 	public static void addToArchiveV2(ProcessFactory processFactory, String functionDesc,
 									File launchDirectory, String archiveFile, String directoryToArchivate) throws Exception
 	{
+		ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.addToArchive");
+
 		if (Environment.isWindows())
 		{
-			ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.addToArchive");
 			processInfo.addToPathEnv.add(new File("./7-Zip/").getAbsoluteFile().getAbsolutePath());
 			processFactory.createProcess(launchDirectory, processInfo, "CMD", "/C", "7z.exe", "a", "-tzip", archiveFile, directoryToArchivate);
-			if (processInfo.error)
-			{
-				Log.warning(processInfo.getStdout());
-				Log.warning(processInfo.getErrOut());
-				throw new Exception("Ошибка создания архива\r\n" + processInfo.getErrOut());
-			}
 		}
 		else
+			processFactory.createProcess(launchDirectory, processInfo, "zip", "-r", archiveFile, directoryToArchivate);
+
+
+		if (processInfo.error)
 		{
-			zipDirectoryToFile(new File(directoryToArchivate), new File(archiveFile));
+			Log.warning(processInfo.getStdout());
+			Log.warning(processInfo.getErrOut());
+			throw new Exception("Ошибка создания архива\r\n"+processInfo.getErrOut());
 		}
 	}
 
 	public static void extractFromArchive(ProcessFactory processFactory, String functionDesc,
 									File launchDirectory, File archiveFile) throws Exception
 	{
+		ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.extractFromArchive");
+		processInfo.addToPathEnv.add(new File("./7-Zip/").getAbsoluteFile().getAbsolutePath());
+
 		if (Environment.isWindows())
-		{
-			ProcessInfo processInfo = new ProcessInfo(functionDesc, 2, "CMDUtils.extractFromArchive");
-			processInfo.addToPathEnv.add(new File("./7-Zip/").getAbsoluteFile().getAbsolutePath());
 			processFactory.createProcess(launchDirectory, processInfo, "CMD", "/C", "7z.exe", "x", archiveFile.getAbsolutePath());
-			if (processInfo.error)
-			{
-				Log.warning(processInfo.getStdout());
-				Log.warning(processInfo.getErrOut());
-				throw new Exception("Архив не был распакован\r\n" + processInfo.getErrOut());
-			}
-		}
 		else
+			processFactory.createProcess(launchDirectory, processInfo, "unzip", archiveFile.getAbsolutePath());
+			
+		if (processInfo.error)
 		{
-			unzipFile(archiveFile, archiveFile.getParentFile());
-		}
-	}
-
-	private static void zipDirectoryToFile(File sourceDir, File destZip) throws IOException
-	{
-		try (FileOutputStream fos = new FileOutputStream(destZip);
-			 ZipOutputStream zos = new ZipOutputStream(fos))
-		{
-			zipEntry(sourceDir, sourceDir.getParentFile(), zos);
-		}
-	}
-
-	private static void zipEntry(File source, File baseDir, ZipOutputStream zos) throws IOException
-	{
-		File[] files = source.listFiles();
-		if (files == null)
-			return;
-		for (File file : files)
-		{
-			String entryName = baseDir.toURI().relativize(file.toURI()).getPath();
-			if (file.isDirectory())
-			{
-				zos.putNextEntry(new ZipEntry(entryName.endsWith("/") ? entryName : entryName + "/"));
-				zos.closeEntry();
-				zipEntry(file, baseDir, zos);
-			}
-			else
-			{
-				zos.putNextEntry(new ZipEntry(entryName));
-				Files.copy(file.toPath(), zos);
-				zos.closeEntry();
-			}
-		}
-	}
-
-	private static void unzipFile(File zipFile, File destDir) throws IOException
-	{
-		byte[] buffer = new byte[8192];
-		try (ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile)))
-		{
-			ZipEntry entry;
-			while ((entry = zis.getNextEntry()) != null)
-			{
-				File outFile = new File(destDir, entry.getName());
-				if (entry.isDirectory())
-				{
-					outFile.mkdirs();
-				}
-				else
-				{
-					outFile.getParentFile().mkdirs();
-					try (FileOutputStream fos = new FileOutputStream(outFile))
-					{
-						int len;
-						while ((len = zis.read(buffer)) > 0)
-							fos.write(buffer, 0, len);
-					}
-				}
-				zis.closeEntry();
-			}
+			Log.warning(processInfo.getStdout());
+			Log.warning(processInfo.getErrOut());
+			throw new Exception("Архив не был распакован\r\n" + processInfo.getErrOut());
 		}
 	}
 
